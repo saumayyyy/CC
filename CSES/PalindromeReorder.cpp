f365c59054260d16f79a80c097d4ad119ebc4355
Saumayyyy <saumayaggarwal6@gmail.com>
2024-09-24 17:33:14 +0530

    pd

--- CSES/PalindromeReorder.cpp
+++ CSES/PalindromeReorder.cpp
@@ -6,14 +6,37 @@ int main(){
     string s;
     cin>>s;
     int n = s.length();
-    unordered_map<char,int>mpp;
-
-    for(auto &it : s){
-        mpp[it]++;
+    vector<int>freq(26,0);
+    for(int i = 0;i<n;i++){
+        freq[s[i]-'A']++;
     }
-    
-    
-
-
+    int cnt = 0;
+    for(int i = 0;i<26;i++){
+        if(freq[i]&1)cnt++;
+    }
+    if(cnt>1){
+        cout<<"NO SOLUTION"<<endl;
+        return 0;
+    }
+    string ans = "";
+    for(int i = 0;i<26;i++){
+        if(freq[i]&1 ^ 1){
+            for(int j = 0;j<freq[i]/2;j++){
+                ans+= (char)(i+'A');
+            }
+        }
+    }
+    cout<<ans;
+    for(int i = 0;i<26;i++){
+        if(freq[i]&1){
+            for(int j = 0;j<freq[i];j++){
+                cout<<(char)(i+'A');
+            }
+            break;
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    cout<<ans;
+    cout<<endl;
     return 0;
 }
