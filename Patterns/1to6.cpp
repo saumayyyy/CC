3d2f6c7dd2dab53117565b5e9116cbe58066c764
Saumayyyy <saumayaggarwal6@gmail.com>
2024-09-30 13:48:34 +0530

    patterns

---
+++ Patterns/1to6.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main(){
+
+    // for(int i = 0;i<4;i++){
+    //     for(int j = 0;j<4;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i = 0;i<5;i++){
+    //     for(int j = 0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i = 0;i<5;i++){
+    //    for(int j = 0;j<=i;j++){
+    //         cout<<(j+1);
+    //    }
+    //    cout<<endl;
+    // }
+
+    // for(int i = 0;i<5;i++){
+    //    for(int j = 0;j<=i;j++){
+    //         cout<<(i+1);
+    //    }
+    //    cout<<endl;
+    // }
+
+    // for(int i = 4;i>=0;i--){
+    //     for(int j = i;j>=0;j--){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i = 4;i>=0;i--){
+    //     for(int j = i;j>=0;j--){
+    //         cout<<(i-j+1);
+    //     }
+    //     cout<<endl;
+    // }
+
+    
+    return 0;
+}
---
+++ Patterns/7to10.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main(){
+    //Q7
+    //spaces-stars-space
+    // 0-1->2i+1 stars on each row, spaces = (9-stars)/2
+    // 1-3
+    // 2-5
+    // 3-7
+    // for(int i = 0;i<5;i++){
+    //     int spaces = 9 - (2*i+1);
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j = 0;j<2*i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    //Q8
+    // for(int i = 4;i>=0;i--){
+    //     int spaces = 9 - (2*i+1);
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j = 0;j<2*i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // Q9
+    // for(int i = 0;i<5;i++){
+    //     int spaces = 9 - (2*i+1);
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j = 0;j<2*i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i = 4;i>=0;i--){
+    //     int spaces = 9 - (2*i+1);
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j = 0;j<2*i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int j = 0;j<spaces/2;j++){
+    //         cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // Q10
+    // for(int i = 0;i<5;i++){
+    //     for(int j = 0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i = 3;i>=0;i--){
+    //     for(int j = 0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    return 0;
+}
